959c53c9bffe4612abd301fdd8354b6ac44c7d1e
biswa123642 <64695542+biswa123642@users.noreply.github.com>
2024-03-16 14:17:24 +0530

    Update cis-kubelet.sh

--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-total_fail=$(kube-bench run --targets node  --version 1.28 --check 4.2.1,4.2.2 --json | jq -r '.Totals.total_fail')
+total_fail=$(kube-bench run --targets node  --version 1.28 --check 3.1.1,3.1.2 --json | jq -r '.Totals.total_fail')
 
 if [[ "$total_fail" -ne 0 ]];
         then
-                echo "CIS Benchmark Failed Kubelet while testing for 4.2.1, 4.2.2"
+                echo "CIS Benchmark Failed Kubelet while testing for 3.1.1, 3.1.2"
                 exit 1;
         else
-                echo "CIS Benchmark Passed Kubelet for 4.2.1, 4.2.2"
+                echo "CIS Benchmark Passed Kubelet for 3.1.1, 3.1.2"
 fi;

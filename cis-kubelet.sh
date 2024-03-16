36736a25a28df59b4c722d6f355a2fdac3b9cf76
biswa123642 <64695542+biswa123642@users.noreply.github.com>
2024-03-16 14:46:39 +0530

    Update cis-kubelet.sh

--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets master --version 1.19 --check 3.1.1,3.1.2 --json | jq -r '.Totals.total_fail')
+total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --targets node --version 1.28 --check 3.1.1,3.1.2 --json | jq -r '.Totals.total_fail')
 
 if [[ "$total_fail" -ne 0 ]];
         then
